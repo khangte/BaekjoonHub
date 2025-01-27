e3b8ede08df00d3fff310f2973cb40f4f646f0f8
MinHyeok Kang <115693185+khangte@users.noreply.github.com>
2025-01-27 16:50:34 +0900

    [Gold V] Title: AC, Time: 164 ms, Memory: 41040 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -8,6 +8,7 @@ for i in range(t):
     arr = deque(sys.stdin.readline().rstrip()[1:-1].split(',')) # []형식의 숫자 배열 입력
 
     reverse_flag = False
+
     if n == 0:
         arr=[]
     for x in p:
@@ -15,15 +16,14 @@ for i in range(t):
             # arr.reverse() 매번 reverse()를 해주어야되기 때문에 시간초과 남
             reverse_flag = not reverse_flag
         elif x == 'D':
-            if len(arr)==0:
+            if arr:
+                if reverse_flag:
+                    arr.pop()
+                else:
+                    arr.popleft()
+            else:
                 print('error')
                 break
-            else:
-                if arr:
-                    if reverse_flag:
-                        arr.pop()
-                    else:
-                        arr.popleft()
     else:
         if reverse_flag:
             arr.reverse()
