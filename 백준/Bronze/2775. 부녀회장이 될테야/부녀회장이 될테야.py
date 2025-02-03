e26bacd032acb3193bf99b259fb54bf9c82ad523
MinHyeok Kang <115693185+khangte@users.noreply.github.com>
2025-02-03 14:04:55 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -8,8 +8,8 @@ for _ in range(t):
     k = int(input().rstrip()) # 층수
     n = int(input().rstrip()) # 호수
 
-    d = [[0] * 15 for _ in range(50)] # DP 테이블
-    for i in range(1, 15):
+    d = [[0] * (n+1) for _ in range(k+1)] # DP 테이블
+    for i in range(1, n+1):
         d[0][i] = i
 
     for x in range(1, k+1): # 1층부터 k층까지
