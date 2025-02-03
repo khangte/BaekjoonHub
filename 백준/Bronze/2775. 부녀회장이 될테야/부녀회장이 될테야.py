479838956664b3ab7edc2f3eca377bd2454e191f
MinHyeok Kang <115693185+khangte@users.noreply.github.com>
2025-02-03 14:02:58 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,18 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+t = int(input().rstrip())
+
+for _ in range(t):
+    k = int(input().rstrip()) # 층수
+    n = int(input().rstrip()) # 호수
+
+    d = [[0] * 15 for _ in range(50)] # DP 테이블
+    for i in range(1, 15):
+        d[0][i] = i
+
+    for x in range(1, k+1): # 1층부터 k층까지
+        for y in range(1, n+1): #1호부터 n호까지
+            d[x][y] = d[x-1][y] + d[x][y-1]
+    print(d[k][n])
