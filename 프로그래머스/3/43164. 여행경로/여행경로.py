05b2ba77d7c76ec52ce286eed3e678483bbb0e72
MinHyeok Kang <115693185+khangte@users.noreply.github.com>
2025-02-18 21:11:07 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(10000)
+
+
+def dfs(airport, graph, route):
+    while graph[airport]:
+        next_airport = graph[airport].pop(0)
+        dfs(next_airport, graph, route)
+    route.append(airport)
+        
+def solution(tickets):
+    from collections import defaultdict
+    
+    graph = defaultdict(list)
+    for src, dst in sorted(tickets):
+        graph[src].append(dst)
+        
+    route = []
+    
+    dfs("ICN", graph, route)
+    return route[::-1]
