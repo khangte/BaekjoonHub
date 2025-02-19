c3ca6856882894b19a626b6c77237cec5aeb9a17
MinHyeok Kang <115693185+khangte@users.noreply.github.com>
2025-02-19 22:22:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.09 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,5 @@
 from collections import Counter
 
 def solution(participant, completion):
-    counts = Counter(participant)
-    for c in completion:
-        counts[c] -= 1
-    for p in counts:
-        if counts[p] > 0:
-            return p
+    answer = Counter(participant) - Counter(completion)
+    return list(answer.keys())[0]
