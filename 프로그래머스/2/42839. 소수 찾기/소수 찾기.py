555cf1d807a1a1898212ebcc1ac72f31ff110e8f
MinHyeok Kang <115693185+khangte@users.noreply.github.com>
2025-02-21 21:33:37 +0900

    [level 2] Title: 소수 찾기, Time: 6.09 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,28 @@
+import math
+from itertools import permutations
+
+def is_prime_number(x):
+    if x < 2:
+        return False
+    if x == 2:
+        return True
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x%i==0:
+            return False
+    return True
+
+def solution(numbers):
+    from itertools import permutations
+    number_list = list(numbers)
+    permutation_set = set()
+    
+    for i in range(1, len(number_list)+1):
+        for p in permutations(number_list, i):
+            num = int(''.join(p))
+            permutation_set.add(num)
+            
+    permutation_set.discard(0)
+    permutation_set.discard(1)
+    
+    cnt = sum(1 for x in permutation_set if is_prime_number(x))
+    return cnt
