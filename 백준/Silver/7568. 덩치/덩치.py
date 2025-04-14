d841686497c5fbe5e1954ddf39810f05693a0f61
MinHyeok Kang <115693185+khangte@users.noreply.github.com>
2025-04-14 17:47:35 +0900

    Update 덩치.py

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -10,11 +10,11 @@ for i in range(n):
     data.append((a, b))
 
 for i in range(n):
-    count = 0
+    rank = 0
     for j in range(n):
         if data[i][0] < data[j][0] and data[i][1] < data[j][1]:
-            count += 1
-    ans.append(count + 1)
+            rank += 1
+    ans.append(rank + 1)
 
 for d in ans:
     print(d, end=' ')
