d216deb8fb84aff0b2ba6f9dde113715e668aeb9
MinHyeok Kang <115693185+khangte@users.noreply.github.com>
2026-03-30 22:52:22 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,28 +1,27 @@
-with a as (
+with rental_base as (
     select 
         h.history_id
         , h.car_id
         , c.car_type
+        , c.daily_fee
         , datediff(h.end_date, h.start_date)+1 as rental_duration
-        , case
-            when datediff(h.end_date, h.start_date)+1 >=90 then '90일 이상'
-            when datediff(h.end_date, h.start_date)+1 >=30 then '30일 이상'
-            when datediff(h.end_date, h.start_date)+1 >=7 then '7일 이상'
-        end as duration_type
-        , c.daily_fee * (datediff(h.end_date, h.start_date)+1) as og_fee
     from car_rental_company_rental_history as h
     join car_rental_company_car as c
     on h.car_id = c.car_id
     where c.car_type = '트럭'
 )
-
-select a.history_id
-    , case
-        when a.duration_type is not null then round(a.og_fee*(100-p.discount_rate)/100, 0)
-        else a.og_fee
-    end as fee
-from a as a
+select rb.history_id
+    , round(
+        rb.daily_fee * rb.rental_duration * (100-coalesce(p.discount_rate, 0))/100
+        , 0
+    ) as fee
+from rental_base as rb
 left join car_rental_company_discount_plan as p
-on a.car_type = p.car_type and a.duration_type = p.duration_type
-order by fee desc, a.history_id desc
+on rb.car_type = p.car_type 
+    and p.duration_type = case
+        when rb.rental_duration>=90 then '90일 이상'
+        when rb.rental_duration>=30 then '30일 이상'
+        when rb.rental_duration>=7 then '7일 이상'
+    end
+order by fee desc, rb.history_id desc
 ;
